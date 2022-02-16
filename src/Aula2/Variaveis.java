7abfec408bc8304ca1ab3811b49012a654407a89
profbreno <brenod123@gmail.com>
2022-02-16 11:50:21 -0300

    Aula 2

---
+++ src/Aula2/Variaveis.java
@@ -0,0 +1,17 @@
+package Aula2;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 6;
+        double media;
+        boolean aprovado;
+        String nome;
+        char conceito;
+
+        media = (nota1 + nota2) / 2;
+
+        System.out.println("As notas foram: " + nota1 + " e " + nota2);
+        System.out.println("A média final foi: " + media);
+    }
+}
